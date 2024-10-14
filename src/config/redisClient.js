de4082b6862817ec2ee4338bf8d3aed9d81554ba
Alex Moreira <amoreira@allianceconsultoria.com.br>
2024-10-14 17:44:03 -0300

    deploy redis

--- src/config/redisClient.js
+++ src/config/redisClient.js
@@ -1,10 +1,20 @@
+require('dotenv').config();
 const redis = require("redis");
-const client = redis.createClient();
+
+const client = redis.createClient({
+  url: process.env.REDIS_URL,
+});
 
 client.on("error", (err) => {
   console.error("Redis error:", err);
 });
 
-client.connect();
+client.connect()
+  .then(() => {
+    console.log('Connected to Redis');
+  })
+  .catch((err) => {
+    console.error('Failed to connect to Redis:', err);
+  });
 
 module.exports = client;
